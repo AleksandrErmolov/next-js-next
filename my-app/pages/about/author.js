883b34620826b5b297d0d0a8fae1d666205371eb
Aleksandr Ermolov <90044699+AleksandrErmolov@users.noreply.github.com>
2022-02-14 20:34:02 +0300

    add fetch

--- my-app/pages/about/author.js
+++ my-app/pages/about/author.js
@@ -1,5 +1,10 @@
+import MainLayout from "../../components/MainLayout";
+
 export default function Author () {
     return (
+        <MainLayout>
         <h1>Author Page</h1>
+        </MainLayout>
+
     )
 }
--- my-app/pages/post/[id].js
+++ my-app/pages/post/[id].js
@@ -1,14 +1,61 @@
+
+import {useState, useEffect}  from "react";
 import {useRouter} from "next/router";
 import MainLayout from "../../components/MainLayout";
+import Link from 'next/link'
 
-export default function Post() {
+
+
+export default function Post({ post: serverPost}) {
 
     const router = useRouter()
-    console.log(router)
+    const [post, setPosts] = useState(serverPost)
+    useEffect(() => {
+        async function load() {
+            const response = await fetch(`http://localhost:4200/posts/${router.query.id}`)
+            const post = await response.json()
+            setPosts(post)
+        }
+
+        if(!post)
+            load()
+
+    },[])
+
+
+
+    if(!post) {
+        return (
+            <MainLayout>
+                <p>loading</p>
+            </MainLayout>
+        )
+    }
+
 
     return (
         <MainLayout>
-            <h1>Post {router.query.id}</h1>
+            <h1>{post.title}</h1>
+            <hr/>
+            <p>{post.body}</p>
+            <Link href={`/posts`}><a>Back to posts</a></Link>
         </MainLayout>
     )
+}
+
+
+
+
+
+export async function getServerSideProps(context) {
+    if(!context.req){
+        return {post: null}
+    }
+    const response = await fetch(`http://localhost:4200/posts/${context.query.id}`)
+    const post = await response.json()
+    return {
+        props: {
+            post,
+        },
+    }
 }
